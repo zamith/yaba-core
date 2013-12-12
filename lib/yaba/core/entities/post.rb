6b1f570f8038054f9d53b9ef8b8a65dadbaf4458
Zamith <zamith.28@gmail.com>
2013-12-12 13:04:13 +0000

    First test passing

---
+++ lib/yaba/core/entities/post.rb
@@ -0,0 +1,22 @@
+require "serializers/pipeline"
+
+module Entities
+  class Post
+    attr_reader :id, :body, :title
+
+    def initialize(args = {})
+      @id = args[:id] || args["id"]
+      @body = args[:body] || args.fetch("body", "")
+      @title = args[:title] || args.fetch("title", "")
+      freeze
+    end
+
+    def ==(other)
+      self.value == other.value
+    end
+
+    def value(serializer: Serializers::Pipeline)
+      serializer.new(self).serialize
+    end
+  end
+end
